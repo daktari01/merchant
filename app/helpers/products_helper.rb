cf7e0404b4d807c34c0163ae98f96c3e44191b6d
James Dindi <dindijjames@gmail.com>
2018-01-21 06:25:34 +0300

    Add stock and its dependencies

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -3,4 +3,13 @@ module ProductsHelper
         #format("$%.2f", price)
         number_to_currency price
     end
+    def print_stock(stock)
+        if stock == 0
+            content_tag(:span, "Out of stock", class: "out_stock")
+        elsif stock >= requested
+            content_tag(:span, "In stock (#{stock})", class: "in_stock")
+        elsif stock < requested
+            content_tag(:span, "Insufficient stock (#{stock})", class: "low_stock")
+        end
+    end
 end
