0be27a802e249e4302666bfe974ae6118d57e23b
Daktari <dindijjames@gmail.com>
2018-01-22 11:26:10 +0000

    Add and configure stock validation

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -4,12 +4,18 @@ module ProductsHelper
         number_to_currency price
     end
     def print_stock(stock)
-        if stock == 0
+        if stock > 0
+            content_tag(:span, "In stock (#{stock})", class: "in_stock" )
+        else
             content_tag(:span, "Out of stock", class: "out_stock")
-        elsif stock >= requested
-            content_tag(:span, "In stock (#{stock})", class: "in_stock")
-        elsif stock < requested
-            content_tag(:span, "Insufficient stock (#{stock})", class: "low_stock")
         end
+        
+        # if stock == 0
+        #     content_tag(:span, "Out of stock", class: "out_stock")
+        # elsif stock >= requested
+        #     content_tag(:span, "In stock (#{stock})", class: "in_stock")
+        # elsif stock < requested
+        #     content_tag(:span, "Insufficient stock (#{stock})", class: "low_stock")
+        # end
     end
 end
