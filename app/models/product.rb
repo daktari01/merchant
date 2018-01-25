d6d0096c73e3cf0ddbc449ff6d9a1d463f9264a9
Daktari <dindijjames@gmail.com>
2018-01-25 12:53:05 +0000

    Add validations and refactor order's show.html

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
-    validates_numericality_of :price
-    validates :stock, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    validates :stock, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :image_url, presence:true
 end
