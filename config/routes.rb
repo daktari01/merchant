3eb44c933f600ca28fb6bf5657c25c33bc12ae0c
James Dindi <dindijjames@gmail.com>
2018-01-30 05:36:20 +0300

    Login now works

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match '/auth/:provider/callback', to: 'sessions#create', via: :get
+  match "/login" => redirect("/auth/twitter"), as: :login, via: :get
+  match "/logout" => "sessions#destroy", as: :logout, via: :get
 end
