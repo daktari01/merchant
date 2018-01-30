c2f2bab28a9d841fd01aea8b1be9d742c46c363c
James Dindi <dindijjames@gmail.com>
2018-01-30 04:49:42 +0300

    Add authentication

--- Gemfile
+++ Gemfile
@@ -54,3 +54,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 ruby '2.4.1'
+
+gem "omniauth-twitter"
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :orders
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  match '/auth/:provider/callback', to: 'sessions#create', via: :get
 end
